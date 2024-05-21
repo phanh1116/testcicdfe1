7ce87654368c531e6b6bed4aeae0ed62a17bc1f8
panh20020123 <tranpanh55@gmail.com>
2024-05-21 07:13:26 +0700

    add d

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()], // Use the imported plugin function here
-  base: "/testcicdfe1/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
